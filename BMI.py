c264552ceba5ada0428f69491762f7f6724707b5
AliACode <111080572+AliACode@users.noreply.github.com>
2022-10-01 03:07:35 -0400

    Create BMI.py

---
+++ BMI.py
@@ -0,0 +1,26 @@
+
+heightIN = float(input('Please enter your height in Inches: '))
+weightLB = float(input('Please enter your weight in Pounds: '))
+
+
+def BMI (heightIN, weightLB):
+    bmi = weightLB/(heightIN**2) * 703
+
+    if bmi >= 19 and bmi <= 24:
+       return 'Healthy :)', bmi
+
+    elif bmi >= 25 and bmi <= 29:
+        return 'OVERWEIGHT!', bmi
+
+    elif bmi >= 30 and bmi <= 39:
+        return 'OBESE', bmi
+
+    elif bmi > 39:
+        return 'OBESE ASF', bmi
+
+    elif bmi < 19:
+        return 'STICKMAN', bmi
+        
+
+quote, bmi = BMI(heightIN, weightLB)
+print('your bmi is: {} and you are: {}' .format(bmi, quote))
